0dda478348b7383528a704c0d6a5488e72b58b1b
SufiyaaanShaikh <sufisk2526@gmail.com>
2025-06-02 12:43:37 +0530

    all request allow

--- server.js
+++ server.js
@@ -14,21 +14,11 @@ import { destinationRoutes } from "./routes/destination.routes.js";
 dotenv.config({ path: "./.env" });
 const app = express();
 const allowedOrigins = [
+  "http://localhost:3000",
   "https://explore-mate-live.vercel.app",
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 
 
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
